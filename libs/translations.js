56b6b8a36372ba1d8904fa6845041cd2c7ad8569
Anton Zhauryd <banderas328@yandex.ru>
2018-09-30 16:59:32 +0300

    refactor

--- libs/translations.js
+++ libs/translations.js
@@ -22,68 +22,68 @@ data = {
         ru: "Тестирование"
     },
     "mission-content" :{
-        ru: " Еще одного из нас поймали сегодня, и теперь это в газетах: \"Подросток арестован за скандальное компьютерное преступление\", \"Хакер арестован после вторжения в банк...\". Тупые ублюдки, они все совершенно одинаковые. Но вы со своей примитивной психологией и техническими познаниями 50-х, хоть раз пытались взглянуть на мир глазами хакера? Пробовали вы понять, что движет им, что сделало его самим собой, какие силы могли сформировать его? Я - хакер. Войди в мой мир... Это началось еще в младших классах... Я был умнее, чем большинство других, и эти кретины постоянно доставали меня... Ну вот я и в средней школе. Слушаю, как учителя по пятнадцать раз объясняют, как сокращать дроби. Я уже давно все понял. \"Нет, мисс Смит, этого нет на бумаге. Я сделал это в уме...\" Проклятье. Видимо, придется написать это в тетради. И вот так всегда... Вчера я совершил открытие. Я раздобыл компьютер. Подождите, это же полный кайф! Он делает все, что я хочу. Если он ошибается, то только потому, что я заставил его. Но не потому, что я ему не нравлюсь,или представляю угрозу для него,или он считает меня хитрой задницей,или не любит учить элементарным вещам.\n" +
-        "\n" +
-        "Тупые дети. Они способны только играться в игрушки. Они все одинаковые...\n" +
-        "\n" +
-        "И тогда это случилось... Открылась дверь в мир... Мчусь по телефонной линии, как героин по венам наркомана. Электронный импульс послан. Убежище от ежедневного кошмара найдено... Соединение установлено. Вот оно - то, чему я принадлежу. Я знаю здесь каждого, хотя никогда не встречался с ними, никогда не говорил с ними, может даже никогда больше их не услышу. Я знаю вас всех... Проклятье, они опять повисли на телефоне. Они все одинаковые... Спорю на что угодно, мы все похожи друг на друга. В школе нам приходилось пережевывать элементарные вещи, хотя мы жаждали большего. Те знания, которые вы нам даете, давно утратили свежесть. Нас доставали школьные садисты, остальные просто игнорировали. Те немногие, у которых есть чему научить других, находят в нас внимательных слушателей, но их так же мало, как воды в пустыне. Но теперь у нас свой мир - мир электрона и клавиатуры, мир красоты данных. Мы используем существующие системы и не хотим платим за то, что могло бы быть бесплатным, но принадлежит богатым жлобам - и вы называете нас преступниками. Мы исследуем - и вы называете нас преступниками. Мы ищем новых знаний - и вы называете нас преступниками. Нас не волнует цвет кожи и национальность, мы живем без религиозной вражды - и вы называете нас преступниками. Вы делаете ядерные бомбы, разжигаете войны, убиваете, лжете и пытаетесь заставить нас поверить, что все это для нашей же пользы - да, тогда мы - преступники. Да, я - преступник. Мое преступление - любопытство. Мое преступление - то, что я сужу о людях по тому, что они говорят и думают, а не по тому, как они выглядят. Мое преступление в том, что я - за пределами вашего понимания, и этого вы мне никогда не простите. Я - хакер, и это - мой манифест. Вы можете остановить меня, но нас всех вам не остановить. Потому что мы все похожи. ",
-        en:  "Another one got caught today, it's all over the papers.  \"Teenager\n" +
-        "Arrested in Computer Crime Scandal\", \"Hacker Arrested after Bank Tampering\"...\n" +
-        "        Damn kids.  They're all alike.\n" +
-        "\n" +
-        "        But did you, in your three-piece psychology and 1950's technobrain,\n" +
-        "ever take a look behind the eyes of the hacker?  Did you ever wonder what\n" +
-        "made him tick, what forces shaped him, what may have molded him?\n" +
-        "        I am a hacker, enter my world...\n" +
-        "        Mine is a world that begins with school... I'm smarter than most of\n" +
-        "the other kids, this crap they teach us bores me...\n" +
-        "        Damn underachiever.  They're all alike.\n" +
-        "\n" +
-        "        I'm in junior high or high school.  I've listened to teachers explain\n" +
-        "for the fifteenth time how to reduce a fraction.  I understand it.  \"No, Ms.\n" +
-        "Smith, I didn't show my work.  I did it in my head...\"\n" +
-        "        Damn kid.  Probably copied it.  They're all alike.\n" +
-        "\n" +
-        "        I made a discovery today.  I found a computer.  Wait a second, this is\n" +
-        "cool.  It does what I want it to.  If it makes a mistake, it's because I\n" +
-        "screwed it up.  Not because it doesn't like me...\n" +
-        "                Or feels threatened by me...\n" +
-        "                Or thinks I'm a smart ass...\n" +
-        "                Or doesn't like teaching and shouldn't be here...\n" +
-        "        Damn kid.  All he does is play games.  They're all alike.\n" +
-        "\n" +
-        "        And then it happened... a door opened to a world... rushing through\n" +
-        "the phone line like heroin through an addict's veins, an electronic pulse is\n" +
-        "sent out, a refuge from the day-to-day incompetencies is sought... a board is\n" +
-        "found.\n" +
-        "        \"This is it... this is where I belong...\"\n" +
-        "        I know everyone here... even if I've never met them, never talked to\n" +
-        "them, may never hear from them again... I know you all...\n" +
-        "        Damn kid.  Tying up the phone line again.  They're all alike...\n" +
-        "\n" +
-        "        You bet your ass we're all alike... we've been spoon-fed baby food at\n" +
-        "school when we hungered for steak... the bits of meat that you did let slip\n" +
-        "through were pre-chewed and tasteless.  We've been dominated by sadists, or\n" +
-        "ignored by the apathetic.  The few that had something to teach found us will-\n" +
-        "ing pupils, but those few are like drops of water in the desert.\n" +
-        "\n" +
-        "        This is our world now... the world of the electron and the switch, the\n" +
-        "beauty of the baud.  We make use of a service already existing without paying\n" +
-        "for what could be dirt-cheap if it wasn't run by profiteering gluttons, and\n" +
-        "you call us criminals.  We explore... and you call us criminals.  We seek\n" +
-        "after knowledge... and you call us criminals.  We exist without skin color,\n" +
-        "without nationality, without religious bias... and you call us criminals.\n" +
-        "You build atomic bombs, you wage wars, you murder, cheat, and lie to us\n" +
-        "and try to make us believe it's for our own good, yet we're the criminals.\n" +
-        "\n" +
-        "        Yes, I am a criminal.  My crime is that of curiosity.  My crime is\n" +
-        "that of judging people by what they say and think, not what they look like.\n" +
-        "My crime is that of outsmarting you, something that you will never forgive me\n" +
-        "for.\n" +
-        "\n" +
-        "        I am a hacker, and this is my manifesto.  You may stop this individual,\n" +
-        "but you can't stop us all... after all, we're all alike."
-}
+        ru: "Команда C58 состоит из опытных экспертов и молодых профессионалов с уникальной квалификацией, подтвержденной международными сертификатами. Наша главная задача обеспечить безопасность и сохранить удобство в пользовании вашими системами. Наши специалисты владеют знаниям по более чем 15 направлениям атак таких  как : атаки на веб системы, облачные технологии, внутренние сети узлы, мобильные приложения и устройства на ряду с другими средствами применяемыми в мире хакеров такими как анонимность, стеганография, шифрование. Мы выполняем работу как в ручную так и с помощью разработанных нами средств автоматизации атак которые мы постоянно совершенствуем.",
+        en:  "C58 team consists of experienced experts and young professionals with a unique qualification, confirmed by international certificates. Our main task is to ensure the safety and ease of use when maintaining your systems.Our specialists have knowledge of more than 15 attack vectors such as attacks on a web systems, cloud technologies, intranet hosts, mobile applications and devices along with other means used in the world of hackers, such as anonymity, steganography, encryption. We carry out the work manualy as long as using automated tools to attack developed by us which we constantly improve."
+     },
+    "services" : {
+        ru: "Сервисы ///",
+        en: "Services ///",
+    },
+    "services-content":{
+        ru: "В качестве сервиса мы можем предложить вам аудит и анализ информационной безопасности, тестирование на проникновение, разработку программного обеспечения, анализ безопасности ваших программных продуктов, консультации по информационной безопасности.",
+        en: "We offer you audit and analysis of information security, penetration testing, software development, analysis of the security of your software products, consulting on information security."
+    },
+    "products":{
+        ru : "|Продукты|",
+        en : "|Products|"
+    },
+    "products-search":{
+        ru: "Программное обеспечение  для автоматического поиска уязвимостей для веб серверов и веб приложений.",
+        en: "Software for automatic vulnerability scanning for Web servers and Web applications."
+    },
+    "products-code": {
+        ru: "Внешний и внутренний анализ безопасности под индивидуальные нужды заказчика с финальной отчетностью и документированными рекомендациями по улучшению.",
+        en: "External and internal security analysis for a customer individual needs  with a final reporting and documented recommendations for improvement."
+    },
+    "about": {
+        ru : "/ Детали \\",
+        en :" / About \\"
+    },
+    "focus-key": {
+        ru : "фокус",
+        en : "focus-key"
+    },
+    "focus-value" : {
+        ru : "в каждый отдельный момент времени наши cпецалисты концентрируются именно над одной задачей.",
+        en : "at any given point in time, our specialists are concentrated just on one task."
+    },
+    "expirience-key" : {
+        ru : "опыт",
+        en : "expirience"
+    },
+    "expirience-value" : {
+        ru : "члены команды имеют большой опыт и широкую экспертизу что позволяет глубоко погружаться в каждый вектор атаки.",
+        en : "team members have extensive experience and broad expertise that allows a deep investigation of each vector of attack."
+    },
+    "expertise-key" : {
+        ru : "экспертиза",
+        en : "expertise"
+    },
+    "expertise-value" : {
+        ru : "больше 20 векторов атак. больше 10 языков программирования. правовая база. официальная государственная лицензия.",
+        en : "more than 20 attack vectors. more than 10 programming languages. framework. Official state license."
+    },
+    "collaboration-key" : {
+        ru : "объеденение",
+        en : "collaboration"
+    },
+    "collaboration-value" : {
+        ru : "вы всегда рады новым партнерам :)",
+        en : "we are always looking for new partners :)"
+    }
+
+
+
 
 
 }
